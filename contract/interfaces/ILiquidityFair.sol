bef0e9be743dacc4be16df436118a664ce2a64d5
small-blocks <84011316+small-blocks@users.noreply.github.com>
2021-05-24 08:31:13 +0700

    Delete contract directory

--- contract/interfaces/ILiquidityFair.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.6.12;
-
-interface ILiquidityFair {
-	/*
-	 * Used when migrating to the new LiquidityFair
-	 * in case of a contract with newer features.
-	 */
-	function migrate(address to) external returns(bool);
-}
